40669051a0eee5dffdce0ab521e74ee3d77be479
Dayo <emmanuelagbaje2013@gmail.com>
2022-07-02 15:02:52 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main -print the phrase"programming is like building a multi-lingual puzzle
+ * Return:0 if existed properly,non-zero otherwise
+ */ 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("/"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
