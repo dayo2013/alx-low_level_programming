d9198b22b47e35ef02f4e07f96aad8949f468d62
Dayo <emmanuelagbaje2013@gmail.com>
2022-07-03 16:39:25 -0700

    The success combination in business is: Do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,21 +12,28 @@ int main(void)
 {
 	int n;
 	int i;
+	int h;
 
-	for (n = 0 ; n < 9 ; n++)
+	for (h = 0 ; h != 8 ; h++)
 	{
-		i = n + 1;
-		do {
-			putchar('0' + n);
-			putchar('0' + i);
-			if (n < 8)
-			{
-				putchar(',');
-				putchar(32);
-			}
-			i++;
-		} while (i < 10);
+		for (n = h + 1 ; n != 9 ; n++)
+		{
+			i = n + 1;
+			do {
+				putchar('0' + h);
+				putchar('0' + n);
+				putchar('0' + i);
+				if (h != 7)
+				{
+					putchar(',');
+					putchar(32);
+				}
+				i++;
+			} while (i != 10);
+		}
 	}
 	putchar('\n');
 	return (0);
 }
+
+
