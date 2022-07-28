b973caf0ac0d83ef083ddaf0451e03776ebe9922
Dayo <emmanuelagbaje2013@gmail.com>
2022-07-27 23:14:12 -0700

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * malloc_checked - allocate memory for b
+ * @b: whatever they give you
+ * Return: void pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *pointer = malloc (b);
+
+	if (pointer == NULL)
+	{
+		exit (98);
+	}
+
+	return (pointer);
+}
